3978f421adbad8a875c775960960436d248c9d9b
Asheklm6 <asheklm6@gmail.com>
2023-12-13 17:52:18 +0100

    my asrray length

---
+++ myArray.js
@@ -0,0 +1,7 @@
+// Build a simple loop, that print numbers to the console. from 0 to the lenght of an array.
+let myArray = [10, 20, 30, 40, 50];
+
+for (let i = 0; i <= myArray.length; i++) {
+    console.log(i);
+}
+
